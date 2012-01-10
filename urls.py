898ebf2a8a5ae4ca77e329fee3859563126f9824
Sagar Karandikar <apps@sagark.org>
2012-01-10 15:12:18 +0000

    switched main map page to bootstrap css

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from campenport.views import homemap, map2, newhomemap
+from campenport.views import homemap
 import campenport.settings as settings
 
 # Uncomment the next two lines to enable the admin:
@@ -9,8 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	(r'^$', 'campenport.views.passhome'),
 	(r'^map/$', homemap),
-	(r'^newmap/$', newhomemap), #testing bootstrap interface, not currently default
-	(r'^map/campusmap.png', map2),
 	(r'^buildings/(?P<buildname>[a-zA-Z0-9_.-]+)/$', 'campenport.buildings.views.buildings'),
 	(r'^ARDgetData/', include('campenport.ARDgetData.urls')),
 	(r'^site-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.SITE_MEDIA + '/site-media/'}),
