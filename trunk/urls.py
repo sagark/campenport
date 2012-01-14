648918629937a20b11348d6d10b377256f26ae77
karandikarsagar@gmail.com <karandikarsagar@gmail.com@44986dfa-e9c2-9616-649f-6ae3395da215>
2012-01-14 22:42:06 +0000

    a bunch of misc changes, added basic compare

--- trunk/urls.py
+++ trunk/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from campenport.views import homemap, buildinglist, search
+from campenport.views import homemap, buildinglist, search, comparebuildings
 import campenport.settings as settings
 
 # Uncomment the next two lines to enable the admin:
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 	(r'^$', 'campenport.views.passhome'),
 	(r'^map/$', homemap),
 	(r'^buildlist/$', buildinglist),
+	(r'^compare/$', comparebuildings),
 	(r'^buildings/(?P<buildname>[a-zA-Z0-9_.-]+)/$', 'campenport.buildings.views.buildings'),
 	(r'^search/$', search),
 	(r'^ARDgetData/', include('campenport.ARDgetData.urls')),
