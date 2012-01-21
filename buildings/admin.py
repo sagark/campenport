1132017624ae2bed6fe4949e5493b507dc955948
Sagar Karandikar <apps@sagark.org>
2012-01-21 16:57:25 +0000

    dropdown external link disclaimer, a bunch of little changes

--- buildings/admin.py
+++ buildings/admin.py
@@ -3,5 +3,6 @@ from campenport.buildings.models import Building
 
 class BuildingAdmin(admin.ModelAdmin):
 	list_display = ('longname', 'queryid', 'stats_sqft', 'stats_yearBuilt', 'pic', 'map_coords')
+	ordering = ('longname',)
 
 admin.site.register(Building, BuildingAdmin)
