fetcher matches statsCalc.js fixed order of operators (units first now)

dbDaemon/fetcher.py
@@ -18,9 +18,9 @@ profiles = eval(json)
 buildings = { }
 
 for testbuilding in profiles:
-	#if testbuilding['longname'] != 'Soda Hall':
+	#if testbuilding['longname'] != 'Sutardja Dai Hall':
 	#	continue
-	print "fetching: " + testbuilding['longname']
+	print "baseline fetch: " + testbuilding['longname']
 
 	#assemble queryids - pre-subsampled ids are in FINALidA, apply-sum ids are in FINALidB
 	if testbuilding['queryid'] == None or testbuilding['queryid'] == '':
@@ -38,7 +38,7 @@ for testbuilding in profiles:
 
 	#start subsample/baseline querying
 	firsturl = 'http://berkeley.openbms.org/ARDgetData/api/query?'
-	tagQ = "select * where Metadata/Location/Building = '" + testbuilding['FINALidA'] + "' and (Metadata/Extra/Operator like 'baseline-%' or (Metadata/Extra/Operator = 'subsampled sum')) and not (Metadata/Path like '1-hr') and not (Metadata/Extra/Resample like 'subsample-mean-3600') and not (Metadata/Extra/Type like 'steam baseline')"
+	tagQ = "select * where Metadata/Location/Building = '" + testbuilding['FINALidA'] + "' and (Metadata/Extra/Operator like 'baseline-%') and not (Metadata/Extra/Operator like 'subsampled sum') and not (Metadata/Path like '1-hr') and not (Metadata/Extra/Resample like 'subsample-mean-3600') and not (Metadata/Extra/Type like 'steam baseline')"
 
 	result = eval(urllib2.urlopen(firsturl, tagQ).read())
 	#print result
@@ -68,16 +68,34 @@ for building in buildings.keys():
 	#print buildings[building].keys()
 	if 'baseline-01' not in buildings[building].keys():
 		del(buildings[building])
+	else:
+		try:
+			temp = np.array(buildings[building]['baseline-01'])
+			buildings[building]['mean'] = np.mean(temp[0:,1])
+			print buildings[building]['mean']
+		except:
+			print 'ERR'
+
 
 #print "discards buildings with no baseline"
 for building in buildings.keys():
 	if 'subsampled sum' not in buildings[building].keys() or buildings[building]['subsampled sum']==[]:
-		print "WARNING: " + building
-		applySumdata = "apply nansum(axis=1) < paste < window(first, field='minute', increment=15) to  data in (" + str(starttime) + ", " + str(currenttime) + ") streamlimit 10000 where Metadata/Extra/System = 'electric'  and ((Properties/UnitofMeasure = 'kW' or Properties/UnitofMeasure = 'Watts') or Properties/UnitofMeasure = 'W') and Metadata/Location/Building like '" + buildings[building]['origtag'] + "%' and not Metadata/Extra/Operator like 'sum%' and not Path like '%demand'"
+		print "actual fetch: " + building
+		applySumdata = "apply nansum(axis=1) < paste < window(first, field='minute', increment=15) < units to  data in (" + str(starttime) + ", " + str(currenttime) + ") streamlimit 10000 where Metadata/Extra/System = 'electric'  and ((Properties/UnitofMeasure = 'kW' or Properties/UnitofMeasure = 'Watts') or Properties/UnitofMeasure = 'W') and Metadata/Location/Building like '" + buildings[building]['origtag'] + "%' and not Metadata/Extra/Operator like 'sum%' and not Path like '%demand'"
 		applySumURL = 'http://berkeley.openbms.org/ARDgetData/api/query?'
 		result3 = eval(urllib2.urlopen(applySumURL, applySumdata).read())
 		result3 = result3[0]['Readings']
 		buildings[building]['subsampled sum'] = result3
+		#try:
+	#		temp = np.array(result3)
+	#		buildings[building]['mean'] = np.mean(temp[0:,1])
+	#		print buildings[building]['mean']
+	#	except:
+	#		print 'ERR'
+#
+
+
+
 		#print result3
 	#print building
 	#print buildings[building].keys()
@@ -113,7 +131,7 @@ def bucketize(inputdata, starttime, endtime):
 
 
 def cleanEdges(averaged1, averaged2):
-	if(len(averaged1)>len(averaged2)):
+	"""if(len(averaged1)>len(averaged2)):
 		flipped = False
 	else:
 		avgtemp = averaged1
@@ -128,7 +146,16 @@ def cleanEdges(averaged1, averaged2):
 		return [averaged2, averaged1]
 	else:
 		return [averaged1, averaged2]
-
+	"""
+	while averaged1[0][0]<averaged2[0][0]:
+		averaged2.pop(0)
+	while averaged1[0][0]>averaged2[0][0]:
+		averaged1.pop(0)
+	while averaged1[len(averaged1)-1][0]<averaged2[len(averaged2)-1][0]:
+		averaged1.pop() 
+	while averaged1[len(averaged1)-1][0]>averaged2[len(averaged2)-1][0]:
+		averaged2.pop()
+	return[averaged1, averaged2]
 
 
 #bin data
@@ -139,14 +166,12 @@ def cleanEdges(averaged1, averaged2):
 #store to DB
 for building in buildings.keys():
 	for datatype in buildings[building].keys():
-		if datatype != 'origtag':
+		if datatype not in ['origtag', 'mean']:
 			buildings[building][datatype] = bucketize(buildings[building][datatype], starttime, currenttime)
 	try:
 		buildings[building]['subsampled sum'], buildings[building]['baseline-01'] = cleanEdges(buildings[building]['subsampled sum'], buildings[building]['baseline-01'])
 	except:
 		pass
-	print len(buildings[building]['subsampled sum'])
-	print len(buildings[building]['baseline-01'])
 
 #print buildings
 
@@ -157,12 +182,13 @@ for building in buildings.keys():
 	pred = buildings[building]['baseline-01']
 	total = 0
 	counter = 0
+	print actual
+	print pred
 	try:
 		for x in range(len(actual)):
 			total += (actual[x][1]-pred[x][1])**2
 			counter += 1
 		rms = total/counter
-	
 	except ZeroDivisionError:
 		rms = 0
 	except:
@@ -171,13 +197,13 @@ for building in buildings.keys():
 	
 	rms = math.sqrt(rms)
 	buildings[building]['rms'] = int(rms*100)/100.0
-	print rms
+	print "rms: " + str(rms)
 	try:
-		temp = np.array(actual)
-		mean = np.mean(temp[0:,1])
-		rmsPercentErr = int(rms/mean*10000)/100.00
+		#temp = np.array(actual)
+		#mean = np.mean(temp[0:,1])
+		rmsPercentErr = int(rms/buildings[building]['mean']*10000)/100.00
 		buildings[building]['rmsPercentErr'] = rmsPercentErr
-		print rmsPercentErr
+		print "rms%err: " + str(rmsPercentErr)
 	except:
 		buildings[building]['rmsPercentErr'] = 0
 	