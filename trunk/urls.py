0cd62406cdd706ed9f00f63916f5256977e4aff6
karandikarsagar@gmail.com <karandikarsagar@gmail.com@44986dfa-e9c2-9616-649f-6ae3395da215>
2012-04-14 07:00:01 +0000

    added versus page, reorganized menus -everything should work on versus, more testing tmrw.

--- trunk/urls.py
+++ trunk/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from campenport.views import homemap, buildinglist, search, comparebuildings, campus
+from campenport.views import homemap, buildinglist, search, comparebuildings, campus, versus
 from campenport.contact.views import contact, contactThanks, contact2
 import campenport.settings as settings
 
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
 	(r'^campus/$', campus),
 	(r'^buildings/(?P<buildname>[a-zA-Z0-9_.-]+)/$', 'campenport.buildings.views.buildings'),
 	(r'^search/$', search),
+	(r'^versus/$', versus),
 	(r'^ARDgetData/', include('campenport.ARDgetData.urls')),
 	(r'^site-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.SITE_MEDIA + '/site-media/'}),
 	(r'^admin/', include(admin.site.urls)),
