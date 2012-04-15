366ff09970f1a5062fb5deb05de61147d1c72fd5
karandikarsagar@gmail.com <karandikarsagar@gmail.com@44986dfa-e9c2-9616-649f-6ae3395da215>
2012-04-15 21:14:40 +0000

    added robots.txt

--- trunk/urls.py
+++ trunk/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from campenport.views import homemap, buildinglist, search, comparebuildings, campus, versus
+from campenport.views import homemap, buildinglist, search, comparebuildings, campus, versus, robots
 from campenport.contact.views import contact, contactThanks, contact2
 import campenport.settings as settings
 
@@ -20,6 +20,7 @@ urlpatterns = patterns('',
 	(r'^ARDgetData/', include('campenport.ARDgetData.urls')),
 	(r'^site-media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.SITE_MEDIA + '/site-media/'}),
 	(r'^admin/', include(admin.site.urls)),
+	(r'^robots.txt', robots),
 	(r'contact/thanks/', contactThanks),
 	(r'^contact/in/(?P<buildnameC>[a-zA-Z0-9_.-]+)/$', contact),
 	(r'^contact/in/', contact2),
