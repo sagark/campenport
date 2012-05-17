207f7dd9b263d349550ddd35ab78cdf1de98ece2
Sagar Karandikar <apps@sagark.org>
2012-05-17 17:12:23 +0000

    implemented peak/avg metric

--- dbDaemon/fetcher.py
+++ dbDaemon/fetcher.py
@@ -3,6 +3,7 @@ import time
 import math
 import numpy as np
 import sqlite3
+import copy
 #change sagar to ubuntu upon deployment
 conn = sqlite3.connect('/home/sagar/dev/campenportdb/buildinginfo.db')
 
@@ -18,7 +19,7 @@ profiles = eval(json)
 buildings = { }
 
 for testbuilding in profiles:
-	#if testbuilding['longname'] != 'Sutardja Dai Hall':
+	#if testbuilding['longname'] != 'Soda Hall':
 	#	continue
 	print "baseline fetch: " + testbuilding['longname']
 
@@ -49,36 +50,20 @@ for testbuilding in profiles:
 
 
 ##############################################if statement here to only include those with baselines
+	buildings[testbuilding['longname']] = {'origtag': testbuilding['FINALidB']}
 
 	for x in range(len(result)):
-		if x == 0:
-			buildings[testbuilding['longname']] = {'origtag': testbuilding['FINALidB']}
+		#if x == 0:
 		secondurl = "http://berkeley.openbms.org/ARDgetData/api/data/uuid/" + result[x]['uuid'] + "?starttime=" + str(starttime) + "&endtime=" + str(currenttime)
 
 		result2 = eval(urllib2.urlopen(secondurl).read())
 		result2 = result2[0]['Readings'] #now the baseline data is stored as a numpy array
 		buildings[testbuilding['longname']][result[x]['Metadata']['Extra']['Operator']] = result2
 		#print result2
-	
 
-#print buildings
-#INVARIANT: buildings only contains buildings that have a baseline, now go through and fetch data for each one that has a baseline but no data
-
+print(buildings.keys())
 for building in buildings.keys():
-	#print buildings[building].keys()
-	if 'baseline-01' not in buildings[building].keys():
-		del(buildings[building])
-	else:
-		try:
-			temp = np.array(buildings[building]['baseline-01'])
-			buildings[building]['mean'] = np.mean(temp[0:,1])
-			print buildings[building]['mean']
-		except:
-			print 'ERR'
-
-
-#print "discards buildings with no baseline"
-for building in buildings.keys():
+	# fetches actual data for all buildings (used later in all_buildings)
 	if 'subsampled sum' not in buildings[building].keys() or buildings[building]['subsampled sum']==[]:
 		print "actual fetch: " + building
 		applySumdata = "apply nansum(axis=1) < paste < window(first, field='minute', increment=1) < units to  data in (" + str(starttime) + ", " + str(currenttime) + ") streamlimit 10000 where Metadata/Extra/System = 'electric'  and ((Properties/UnitofMeasure = 'kW' or Properties/UnitofMeasure = 'Watts') or Properties/UnitofMeasure = 'W') and Metadata/Location/Building like '" + buildings[building]['origtag'] + "%' and not Metadata/Extra/Operator like 'sum%' and not Path like '%demand'"
@@ -86,19 +71,51 @@ for building in buildings.keys():
 		result3 = eval(urllib2.urlopen(applySumURL, applySumdata).read())
 		result3 = result3[0]['Readings']
 		buildings[building]['subsampled sum'] = result3
-		#try:
-	#		temp = np.array(result3)
-	#		buildings[building]['mean'] = np.mean(temp[0:,1])
-	#		print buildings[building]['mean']
-	#	except:
-	#		print 'ERR'
+
+all_buildings = copy.deepcopy(buildings)
+#performs operations for all buildings
+for building in all_buildings.keys():
+	print("current building is: " + str(building))
+	try:
+		temp = np.array(all_buildings[building]['subsampled sum'])
+		actual_mean = np.mean(temp[0:,1])
+		actual_max = np.amax(temp[0:,1])
+		all_buildings[building]['peak_avg'] = int(actual_max/actual_mean*100)/100.0
+	except:
+		all_buildings[building]['peak_avg'] = 0.0
+		print(all_buildings[building]['subsampled sum'])
+		asdf = raw_input("Press Enter to continue...")
+	print("peak to avg: " + str(all_buildings[building]['peak_avg']))
 #
 
+buildings2 = {}
+
+for building in buildings.keys():
+	if 'baseline-01' in buildings[building].keys():
+		buildings2[building] = buildings[building]
+
+buildings = {}
+buildings = buildings2
 
 
-		#print result3
-	#print building
-	#print buildings[building].keys()
+#####################################################
+for building in buildings.keys():
+	# this removes buildings without a baseline the buildings dict
+	if 'baseline-01' not in buildings[building].keys():
+		#del(buildings[building])
+		pass
+	else:
+		try:
+			temp = np.array(buildings[building]['baseline-01'])
+			buildings[building]['mean'] = np.mean(temp[0:,1])
+			print buildings[building]['mean']
+		except:
+			print 'ERR'
+######################################################
+# from this point on, buildings contains only buildings with a baseline and
+# all_buildings contains all buildings
+
+
 
 #now that all data is available:
 
@@ -131,22 +148,6 @@ def bucketize(inputdata, starttime, endtime):
 
 
 def cleanEdges(averaged1, averaged2):
-	"""if(len(averaged1)>len(averaged2)):
-		flipped = False
-	else:
-		avgtemp = averaged1
-		averaged1 = averaged2
-		averaged2 = avgtemp
-		flipped = True
-	while averaged1[0][0]!=averaged2[0][0]:
-		averaged1.pop(0)
-	while averaged1[len(averaged1)-1][0]!=averaged2[len(averaged2)-1][0]:
-		averaged1.pop()
-	if flipped:
-		return [averaged2, averaged1]
-	else:
-		return [averaged1, averaged2]
-	"""
 	while averaged1[0][0]<averaged2[0][0]:
 		averaged2.pop(0)
 	while averaged1[0][0]>averaged2[0][0]:
@@ -173,19 +174,24 @@ for building in buildings.keys():
 	except:
 		pass
 
-#print buildings
 
-
-#####now compute rms:
+#####now compute rms, peak/avg:
 for building in buildings.keys():
 	actual = buildings[building]['subsampled sum']
 	pred = buildings[building]['baseline-01']
 	total = 0
 	counter = 0
-	print actual
-	print pred
+	maxSoFar = 0
+	#stand_mean_numer = 0
+	#stand_mean_denom = 0
+	#print actual
+	#print pred
 	try:
 		for x in range(len(actual)):
+			if actual[x][1]>maxSoFar:
+				maxSoFar = actual[x][1]
+			#stand_mean_numer += actual[x][1]
+			#stand_mean_denom += 1
 			total += (actual[x][1]-pred[x][1])**2
 			counter += 1
 		rms = total/counter
@@ -195,9 +201,17 @@ for building in buildings.keys():
 		print("forloopissue")
 		rms = 0
 	
+	#if stand_mean_denom != 0:
+	#	stand_mean = int(stand_mean_numer/stand_mean_denom*100)/100.0
+	#else:
+	#	stand_mean = 0
 	rms = math.sqrt(rms)
 	buildings[building]['rms'] = int(rms*100)/100.0
+	#buildings[building]['mean'] = stand_mean
+	buildings[building]['max'] = maxSoFar
+	buildings[building]['peak_avg'] = int(maxSoFar/buildings[building]['mean']*100)/100.0
 	print "rms: " + str(rms)
+	print "peak/avg: " + str(buildings[building]['peak_avg'])
 	try:
 		#temp = np.array(actual)
 		#mean = np.mean(temp[0:,1])
@@ -212,7 +226,7 @@ for building in buildings.keys():
 
 
 
-#write to database
+#write to database for baselined buildings
 for building in buildings.keys():
 	cursor = conn.cursor()
 	cursor.execute("UPDATE buildings_building SET rmsDev=" + str(buildings[building]['rms']) + " WHERE longname='" + building + "'")
@@ -220,10 +234,10 @@ for building in buildings.keys():
 	conn.commit()
 	cursor.close()
 
-
-#counter = 0
-#for x in profiles:
-#	counter += 1
-#	print x['longname'] + " " + 
-
-#print counter
+#write to database for all buildings
+for building in all_buildings.keys():
+	cursor = conn.cursor()
+	print("adding to db " + building)
+	cursor.execute("UPDATE buildings_building SET peak_avg=" + str(all_buildings[building]['peak_avg']) + " WHERE longname='" + building + "'")
+	conn.commit()
+	cursor.close()
